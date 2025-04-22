2e5665910166f6c1b615f118641646eefdcc0fa1
Alyasaf Meireles <gasafm62@gmail.com>
2025-04-22 10:44:00 -0300

    feat: primeiro commit

---
+++ src/main/java/com/anhanguera/crescerbem/CrescerbemApplication.java
@@ -0,0 +1,13 @@
+package com.anhanguera.crescerbem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrescerbemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrescerbemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anhanguera/crescerbem/CrescerbemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhanguera.crescerbem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrescerbemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
